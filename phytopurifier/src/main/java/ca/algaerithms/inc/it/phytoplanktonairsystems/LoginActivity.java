45da29381e619eb14adcaaec58390623cf840018
Dharmikshah2611 <shahdharmik.2611@gmail.com>
2025-06-07 17:31:54 -0400

    Logout Screen:      - Added a visibility icon in the password textbox to toggle visibility.      - extracted strings to strings.xml from activity_login.xml      - changed the icon in the login screen similar to the app icon

--- phytopurifier/src/main/java/ca/algaerithms/inc/it/phytoplanktonairsystems/LoginActivity.java
+++ phytopurifier/src/main/java/ca/algaerithms/inc/it/phytoplanktonairsystems/LoginActivity.java
@@ -1,16 +1,19 @@
 package ca.algaerithms.inc.it.phytoplanktonairsystems;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-    //private ActivityLoginScreenBinding binding;
     private Button loginSubmitButton;
 
     @Override
@@ -28,6 +31,40 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        setupPasswordToggle();
 
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void setupPasswordToggle() {
+        EditText passwordEditText = findViewById(R.id.login_Password);
+
+        passwordEditText.setOnTouchListener((v, event) -> {
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                final int DRAWABLE_END = 2;
+                if (passwordEditText.getCompoundDrawables()[DRAWABLE_END] != null) {
+                    int drawableWidth = passwordEditText.getCompoundDrawables()[DRAWABLE_END].getBounds().width();
+                    int touchAreaStart = passwordEditText.getRight() - passwordEditText.getPaddingRight() - drawableWidth;
+
+                    if (event.getRawX() >= touchAreaStart) {
+                        togglePasswordVisibility(passwordEditText);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        });
+    }
+
+    private void togglePasswordVisibility(EditText passwordEditText) {
+        int inputType = passwordEditText.getInputType();
+        if ((inputType & InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD) == InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD) {
+            passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            passwordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.visibility_on, 0);
+        } else {
+            passwordEditText.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+            passwordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.visibility_off, 0);
+        }
+        passwordEditText.setSelection(passwordEditText.getText().length());
     }
 }
