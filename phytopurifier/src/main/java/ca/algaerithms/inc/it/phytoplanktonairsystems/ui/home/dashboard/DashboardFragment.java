b1d68da94e29e04b9c9db16ddd1c938ab098172d
JulianImperial8310 <ja.imperial16@gmail.com>
2025-06-07 21:23:09 -0400

    Added button to switch from Dashboard view to Graph View in DashboardFragment.java along with their appropriate LinearLayouts

--- phytopurifier/src/main/java/ca/algaerithms/inc/it/phytoplanktonairsystems/ui/home/dashboard/DashboardFragment.java
+++ phytopurifier/src/main/java/ca/algaerithms/inc/it/phytoplanktonairsystems/ui/home/dashboard/DashboardFragment.java
@@ -7,12 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ToggleButton;
 
 import ca.algaerithms.inc.it.phytoplanktonairsystems.R;
 import ca.algaerithms.inc.it.phytoplanktonairsystems.databinding.FragmentDashboardBinding;
 
 
 public class DashboardFragment extends Fragment {
+
+    private View dashboardView;
+    private View graphView;
+    private ToggleButton toggleButton;
 
     private FragmentDashboardBinding binding;
 
@@ -22,8 +27,19 @@ public class DashboardFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
+        dashboardView = view.findViewById(R.id.dashboard_view);
+        graphView = view.findViewById(R.id.graph_view);
+        toggleButton = view.findViewById(R.id.toggle_view_button);
 
-
+        toggleButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if(isChecked){
+                dashboardView.setVisibility(View.GONE);
+                graphView.setVisibility(View.VISIBLE);
+            } else {
+                dashboardView.setVisibility(View.VISIBLE);
+                graphView.setVisibility(View.GONE);
+            }
+        });
 
         return view;
     }
