9d22ebbf0522871ceafe08dd0299d7b0137b2c64
JulianImperial8310 <ja.imperial16@gmail.com>
2025-06-08 10:59:21 -0400

    Created UserStat.java for Leaderboard

---
+++ phytopurifier/src/main/java/ca/algaerithms/inc/it/phytoplanktonairsystems/ui/home/leaderboard/UserStat.java
@@ -0,0 +1,11 @@
+package ca.algaerithms.inc.it.phytoplanktonairsystems.ui.home.leaderboard;
+
+public class UserStat {
+    public String name;
+    public int oxygenProducedKg;
+
+    public UserStat(String name, int oxygenProducedKg) {
+        this.name = name;
+        this.oxygenProducedKg = oxygenProducedKg;
+    }
+}
