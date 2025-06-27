513ea091b0d1a15f6a051d583af864c7751277e5
Dharmikshah2611 <shahdharmik.2611@gmail.com>
2025-06-27 19:44:20 -0400

    Registration Activity:     - created a layout with 2 edit text view for the user to enter their first and last name.     - both the fields would need an input to move to the next fragment

--- phytopurifier/src/main/java/ca/algaerithms/inc/it/phytoplanktonairsystems/ui/registration/RegistrationNameFragment.java
+++ phytopurifier/src/main/java/ca/algaerithms/inc/it/phytoplanktonairsystems/ui/registration/RegistrationNameFragment.java
@@ -4,63 +4,66 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import ca.algaerithms.inc.it.phytoplanktonairsystems.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RegistrationNameFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class RegistrationNameFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public RegistrationNameFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RegistrationNameFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RegistrationNameFragment newInstance(String param1, String param2) {
-        RegistrationNameFragment fragment = new RegistrationNameFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private EditText firstNameEditText, lastNameEditText;
+    private Button continueButton;
+    private String email;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration_name, container, false);
+        View view = inflater.inflate(R.layout.fragment_registration_name, container, false);
+
+        firstNameEditText = view.findViewById(R.id.registration_diplayFirstName_editText);
+        lastNameEditText = view.findViewById(R.id.registration_displayLastName_editText);
+        continueButton = view.findViewById(R.id.registration_display_continueButton);
+
+        // Get email from previous fragment
+        if (getArguments() != null) {
+            email = getArguments().getString("email");
+        }
+
+        continueButton.setOnClickListener(v -> {
+            String firstName = firstNameEditText.getText().toString().trim();
+            String lastName = lastNameEditText.getText().toString().trim();
+
+            if (TextUtils.isEmpty(firstName)) {
+                firstNameEditText.setError("First name is required");
+                return;
+            }
+
+            if (TextUtils.isEmpty(lastName)) {
+                lastNameEditText.setError("Last name is required");
+                return;
+            }
+
+            String fullName = firstName + " " + lastName;
+
+            // Pass email and full name to next fragment
+            Bundle bundle = new Bundle();
+            bundle.putString("email", email);
+            bundle.putString("name", fullName);
+
+            RegistrationBirthdateFragment birthdateFragment = new RegistrationBirthdateFragment();
+            birthdateFragment.setArguments(bundle);
+
+            requireActivity().getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.registration_fragment_container, birthdateFragment)
+                    .addToBackStack(null)
+                    .commit();
+        });
+
+        return view;
     }
 }
