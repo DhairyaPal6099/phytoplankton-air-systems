20dd15b17215f1591580f27ebefeb10121813d61
SanskritiMansotra <mansotrasanskriti@gmail.com>
2025-07-20 09:11:31 -0400

    Added custom icons for the team

--- phytopurifier/src/main/java/ca/algaerithms/inc/it/phytoplanktonairsystems/ui/about/AboutFragment.java
+++ phytopurifier/src/main/java/ca/algaerithms/inc/it/phytoplanktonairsystems/ui/about/AboutFragment.java
@@ -5,6 +5,8 @@
 
 package ca.algaerithms.inc.it.phytoplanktonairsystems.ui.about;
 
+import static android.os.Build.VERSION_CODES.R;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,7 +24,7 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_about, container, false);
+        View view = inflater.inflate(R, container, false);
 
 
 
