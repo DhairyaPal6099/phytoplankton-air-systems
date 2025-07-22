1cf057c2ab299f0b4f14c42f801ae3f465a24ed5
JulianImperial8310 <ja.imperial16@gmail.com>
2025-07-21 21:33:05 -0400

    Added Team Member Comments to all Java Classes under ca/algaerithms/inc/it/phytoplanktonairsystems/ui

--- phytopurifier/src/main/java/ca/algaerithms/inc/it/phytoplanktonairsystems/view/ui/settings/PrivacyPolicyFragment.java
+++ phytopurifier/src/main/java/ca/algaerithms/inc/it/phytoplanktonairsystems/view/ui/settings/PrivacyPolicyFragment.java
@@ -1,3 +1,8 @@
+/* Julian Imperial – N01638310
+   Dhairya Pal – N01576099
+   Sanskriti Mansotra – N01523183
+   Dharmik Shah – N01581796 */
+
 package ca.algaerithms.inc.it.phytoplanktonairsystems.view.ui.settings;
 
 import android.os.Bundle;
