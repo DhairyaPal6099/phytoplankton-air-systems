106a8c85469072ee5fa2b134afc5d990b64c2211
DhairyaPal6099 <n01576099@humber.ca>
2025-07-27 10:49:58 -0400

    Wrote a test case to check if the email is prefilled when the feedback screen is opened

--- phytopurifier/src/androidTest/java/ca/algaerithms/inc/it/phytoplanktonairsystems/view/ui/FeedbackFragmentTest.java
+++ phytopurifier/src/androidTest/java/ca/algaerithms/inc/it/phytoplanktonairsystems/view/ui/FeedbackFragmentTest.java
@@ -174,6 +174,14 @@ public class FeedbackFragmentTest {
         };
     }
 
+    @Test
+    public void testEmailIdPrefilled() {
+        loginToApp();
+        openFeedbackFragment();
+
+        onView(withId(R.id.etEmail)).check(matches(isDisplayed())).check(matches(withText("brucershall@gmail.com")));
+    }
+
     private void loginToApp() {
         onView(withId(R.id.login_username)).perform(replaceText("brucershall@gmail.com"), closeSoftKeyboard());
         onView(withId(R.id.login_Password)).perform(replaceText("Malaika1"), closeSoftKeyboard());
