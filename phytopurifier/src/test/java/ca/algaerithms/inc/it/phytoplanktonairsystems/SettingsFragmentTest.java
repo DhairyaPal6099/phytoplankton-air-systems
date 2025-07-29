cc05a1af160f9e921c5d0ff47b390ce80481f339
DhairyaPal6099 <n01576099@humber.ca>
2025-07-29 09:08:55 -0400

    - Created SettingsFragmentTest.java to run Robolectric test cases on SettingsFragment. - Added team members' names and ids' as comments on the Test java files. - Added Robolectric dependencies to the project.

---
+++ phytopurifier/src/test/java/ca/algaerithms/inc/it/phytoplanktonairsystems/SettingsFragmentTest.java
@@ -0,0 +1,20 @@
+/* Julian Imperial – N01638310
+   Dhairya Pal – N01576099
+   Sanskriti Mansotra – N01523183
+   Dharmik Shah – N01581796 */
+
+package ca.algaerithms.inc.it.phytoplanktonairsystems;
+
+import android.os.Build;
+
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = Build.VERSION_CODES.R)
+public class SettingsFragmentTest {
+
+}
