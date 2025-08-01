19fd336a45544e688be422bbfbe56a8e791d1e49
Dharmikshah2611 <shahdharmik.2611@gmail.com>
2025-08-01 13:49:27 -0400

    Settings Fragment:      - Removed unwanted imports from the fragment.

--- phytopurifier/src/main/java/ca/algaerithms/inc/it/phytoplanktonairsystems/view/ui/settings/SettingsFragment.java
+++ phytopurifier/src/main/java/ca/algaerithms/inc/it/phytoplanktonairsystems/view/ui/settings/SettingsFragment.java
@@ -5,18 +5,11 @@
 
 package ca.algaerithms.inc.it.phytoplanktonairsystems.view.ui.settings;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.Switch;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -24,9 +17,6 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 import ca.algaerithms.inc.it.phytoplanktonairsystems.R;
 import ca.algaerithms.inc.it.phytoplanktonairsystems.controller.SettingsController;
