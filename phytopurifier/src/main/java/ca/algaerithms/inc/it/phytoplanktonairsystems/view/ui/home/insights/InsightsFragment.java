78767061d7cc79e103967ed850d27eef07b4ad47
Dharmikshah2611 <shahdharmik.2611@gmail.com>
2025-08-03 11:26:24 -0400

    Insights fragment:      - Created a layout for videos and added test videos for testing.

--- phytopurifier/src/main/java/ca/algaerithms/inc/it/phytoplanktonairsystems/view/ui/home/insights/InsightsFragment.java
+++ phytopurifier/src/main/java/ca/algaerithms/inc/it/phytoplanktonairsystems/view/ui/home/insights/InsightsFragment.java
@@ -4,9 +4,12 @@
    Dharmik Shah – N01581796 */
 package ca.algaerithms.inc.it.phytoplanktonairsystems.view.ui.home.insights;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,14 +18,16 @@ import android.webkit.WebView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import ca.algaerithms.inc.it.phytoplanktonairsystems.R;
 import ca.algaerithms.inc.it.phytoplanktonairsystems.databinding.FragmentInsightsBinding;
 
 
 public class InsightsFragment extends Fragment {
-    private Spinner videoSpinner;
-    private WebView videoWebView;
-
+    private RecyclerView basicInsights, intermediateInsights, advancedInsights;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,34 +35,42 @@ public class InsightsFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_insights, container, false);
 
-        videoSpinner = view.findViewById(R.id.video_spinner);
-        videoWebView = view.findViewById(R.id.video_webview);
+        basicInsights = view.findViewById(R.id.basicInsightsRecycler);
+        intermediateInsights = view.findViewById(R.id.intermediateInsightsRecycler);
+        advancedInsights = view.findViewById(R.id.advancedInsightsRecycler);
 
-        String[] videoTitles = getResources().getStringArray(R.array.video_titles);
-        String[] videoUrls = getResources().getStringArray(R.array.video_urls);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(),
-                android.R.layout.simple_spinner_item, videoTitles);
-        videoSpinner.setAdapter(adapter);
-
-        videoWebView.getSettings().setJavaScriptEnabled(true);
-
-        videoSpinner.setOnItemSelectedListener(new android.widget.AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(android.widget.AdapterView<?> parent, View view, int position, long id) {
-                String videoUrl = videoUrls[position];
-                String html = "<html><body style='margin:0'><iframe width=\"100%\" height=\"100%\" src=\"" +
-                        videoUrl + "\" frameborder=\"0\" allowfullscreen></iframe></body></html>";
-                videoWebView.loadData(html, "text/html", "utf-8");
-            }
-
-            @Override
-            public void onNothingSelected(android.widget.AdapterView<?> parent) {
-                // Do nothing
-            }
-        });
-
+        setupVideoRecyclerViews();
 
         return view;
+    }
+
+    private List<VideoItem> loadVideosFromResources(int titlesResId, int idsResId) {
+        String[] titles = getResources().getStringArray(titlesResId);
+        String[] ids = getResources().getStringArray(idsResId);
+
+        List<VideoItem> list = new ArrayList<>();
+        for (int i = 0; i < Math.min(titles.length, ids.length); i++) {
+            list.add(new VideoItem(titles[i], ids[i]));
+        }
+        return list;
+    }
+
+    private void setupVideoRecyclerViews() {
+        Context ctx = getContext();
+
+        // Basic Insights
+        basicInsights.setLayoutManager(new LinearLayoutManager(ctx, LinearLayoutManager.HORIZONTAL, false));
+        List<VideoItem> phytoList = loadVideosFromResources(R.array.basic_titles, R.array.basic_ids);
+        basicInsights.setAdapter(new VideoAdapter(ctx, phytoList));
+
+        // Intermediate Insights
+        intermediateInsights.setLayoutManager(new LinearLayoutManager(ctx, LinearLayoutManager.HORIZONTAL, false));
+        List<VideoItem> bruhList = loadVideosFromResources(R.array.intermediate_titles, R.array.intermediate_ids);
+        intermediateInsights.setAdapter(new VideoAdapter(ctx, bruhList));
+
+        // Advanced Insights
+        advancedInsights.setLayoutManager(new LinearLayoutManager(ctx, LinearLayoutManager.HORIZONTAL, false));
+        List<VideoItem> omgList = loadVideosFromResources(R.array.advanced_titles, R.array.advanced_ids);
+        advancedInsights.setAdapter(new VideoAdapter(ctx, omgList));
     }
 }
