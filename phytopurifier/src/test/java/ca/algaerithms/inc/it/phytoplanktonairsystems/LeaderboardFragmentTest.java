37ff8592256e03e636c9ff64875830eead0ef6f8
JulianImperial8310 <ja.imperial16@gmail.com>
2025-08-03 21:23:26 -0400

    First Test case for LeaderboardFragmentTest.java, for adapter initializing correctly with data

--- phytopurifier/src/test/java/ca/algaerithms/inc/it/phytoplanktonairsystems/LeaderboardFragmentTest.java
+++ phytopurifier/src/test/java/ca/algaerithms/inc/it/phytoplanktonairsystems/LeaderboardFragmentTest.java
@@ -1,12 +1,33 @@
 package ca.algaerithms.inc.it.phytoplanktonairsystems;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import ca.algaerithms.inc.it.phytoplanktonairsystems.view.adapter.LeaderboardAdapter;
+import ca.algaerithms.inc.it.phytoplanktonairsystems.view.ui.home.leaderboard.UserStat;
 
 public class LeaderboardFragmentTest {
 
+    Context context = ApplicationProvider.getApplicationContext();
+
+
     @Test
+    public void adapter_initializes_with_given_user_stats() {
+        List<UserStat> mockList = Arrays.asList(new UserStat("Julian", 12.5));
+        LeaderboardAdapter adapter = new LeaderboardAdapter(context, mockList);
+        assertEquals(1, adapter.getItemCount());
+    }
+
 
 }
