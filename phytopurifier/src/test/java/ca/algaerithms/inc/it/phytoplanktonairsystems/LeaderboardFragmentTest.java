78acca53ec8a22ce738e400239eda0a656b3482a
JulianImperial8310 <ja.imperial16@gmail.com>
2025-08-03 21:18:59 -0400

    Refactored ShareDashboardTest.java to LeaderboardFragmentTest.java

--- phytopurifier/src/test/java/ca/algaerithms/inc/it/phytoplanktonairsystems/LeaderboardFragmentTest.java
+++ phytopurifier/src/test/java/ca/algaerithms/inc/it/phytoplanktonairsystems/LeaderboardFragmentTest.java
@@ -4,5 +4,9 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-public class ShareDashboardTest {
+
+public class LeaderboardFragmentTest {
+
+    @Test
+
 }
